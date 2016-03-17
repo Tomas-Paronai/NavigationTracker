82773108a9222f469b393f0c332b967d643c9ad7
Tomas-Paronai <tomas.paronai@akademiasovy.sk>
2016-03-17 10:48:47 +0100

    update

--- app/src/main/java/parohyapp/navigationtracker/MainMapActivity.java
+++ app/src/main/java/parohyapp/navigationtracker/MainMapActivity.java
@@ -1,32 +1,39 @@
 package parohyapp.navigationtracker;
 
-import android.content.Context;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import parohyapp.navigationtracker.fragments.Settings;
-import parohyapp.navigationtracker.handler.localization.DeviceLocationListener;
 import parohyapp.navigationtracker.handler.localization.PositionListener;
-import parohyapp.navigationtracker.handler.marker.MarkerBLE;
 import parohyapp.navigationtracker.handler.marker.MarkerHandler;
 
-public class MainMapActivity extends FragmentActivity implements OnMapReadyCallback,PositionListener,GoogleMap.OnMarkerClickListener {
+public class MainMapActivity extends FragmentActivity implements OnMapReadyCallback,
+        PositionListener,
+        GoogleMap.OnMarkerClickListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private final static String TAG = "MainActivity";
 
     private GoogleMap mMap;
+    private GoogleApiClient gApiClient;
     private LatLng myCords;
 
     private MarkerHandler markerHandler;
@@ -42,8 +49,10 @@ public class MainMapActivity extends FragmentActivity implements OnMapReadyCallb
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        settingsFragment = (Settings) getSupportFragmentManager().findFragmentById(R.id.settings_fragment);
-        settingsFragment.toggle();
+//        settingsFragment = (Settings) getSupportFragmentManager().findFragmentById(R.id.settings_fragment);
+//        settingsFragment.toggle();
+
+        gApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
     }
 
 
@@ -51,44 +60,17 @@ public class MainMapActivity extends FragmentActivity implements OnMapReadyCallb
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        mMap.getUiSettings().setCompassEnabled(false);
         markerHandler = new MarkerHandler(googleMap);
 
-        Location myLocation = getDeviceLocation();
-        updateMyLocation(myLocation.getLatitude(), myLocation.getLongitude());
-
-        if(myCords != null){
-            if(myMarker!=null){
-                myMarker.remove();
-            }
-            //myMarker = mMap.addMarker(new MarkerOptions().position(myCords).title("You"));
-
-            myMarker = markerHandler.addMarker(myCords,"You");
-            mMap.moveCamera(CameraUpdateFactory.zoomTo(13.5f));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(myCords));
-        }
-
-        //testing dummy
-        MarkerBLE marker1 = new MarkerBLE(new LatLng(myCords.latitude+1,myCords.longitude+1),1,"Marker1");
-        markerHandler.addMarkerBLE(marker1);
-    }
-
-    private Location getDeviceLocation(){
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        LocationListener mlocListener = new DeviceLocationListener(this);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
-        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
     }
 
     @Override
     public void updateMyLocation(double latitude, double longitude) {
-        myCords = new LatLng(latitude,longitude);
-
-        //TODO needs testing
-        if(myMarker!=null){
-            myMarker.remove();
-        }
-        myMarker = markerHandler.addMarker(myCords,"You");
+        Toast.makeText(this,"Lat: "+latitude+" Lon: "+longitude,Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"Lat: "+latitude+" Lon: "+longitude);
     }
 
     @Override
@@ -97,16 +79,25 @@ public class MainMapActivity extends FragmentActivity implements OnMapReadyCallb
     }
 
     public void resetCamera(View v){
-        if(myCords != null){
-            mMap.moveCamera(CameraUpdateFactory.zoomTo(13.5f));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(myCords));
-        }
+
     }
 
     public void openSettings(View v){
         //TODO style settings
         //TODO implement settings listener
-        settingsFragment.toggle();
+
+    }
+
+    @Override
+    protected void onStart() {
+        gApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        gApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
@@ -117,6 +108,36 @@ public class MainMapActivity extends FragmentActivity implements OnMapReadyCallb
 
     /*DEBUG BUTTON*/
     public void myClick(View v){
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(7000);
+        LocationServices.FusedLocationApi.requestLocationUpdates(gApiClient,locationRequest,this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        if(i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST){
+            positionListenerStatus("Network connection lost");
+        }
+        else if(i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED){
+            positionListenerStatus("Connection service disconnected");
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        updateMyLocation(location.getLatitude(),location.getLongitude());
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
 }
